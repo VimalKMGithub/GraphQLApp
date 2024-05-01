23a1a0d02f3fcc242d06a211b30a305a67fad72a
Vimal Kumar Mishra <vimalkumarm472@gmail.com>
2024-05-01 23:22:54 +0530

    .

--- backend/cron.js
+++ backend/cron.js
@@ -1,7 +1,7 @@
 import cron from "cron";
 import https from "https";
 
-const URL = "https://graphqlapp.onrender.com";
+const URL = "https://vgraphql.onrender.com";
 
 const job = new cron.CronJob("*/14 * * * *", function () {
 	https
